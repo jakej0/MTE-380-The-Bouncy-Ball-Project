80a297c718e6f42e5b233d0f82ce9c16a873df01
Eeman Aleem <113636413+EemanAleem@users.noreply.github.com>
2024-06-14 14:37:27 -0400

    General Testing with the TB6600 Stepper Driver

---
+++ MotorDriver/TB6600_Test.ino
@@ -0,0 +1,68 @@
+#include <AccelStepper.h>
+const int dirPin1 = 2;
+const int stepPin1 = 3; //pulsePin
+const int enPin1 = 4;
+
+const int dirPin2 = 5;
+const int stepPin2 = 6; //pulsePin
+const int enPin2 = 7;
+
+const int dirPin3 = 8;
+const int stepPin3 = 9; //pulsePin
+const int enPin3 = 10;
+
+/*const float St_Speed = 9000;
+const float St_Accel = 900;
+AccelStepper O
+*/
+
+void setup() {
+  pinMode(stepPin1, OUTPUT);
+  pinMode(dirPin1, OUTPUT);
+  pinMode(enPin1, OUTPUT);
+
+  pinMode(stepPin2, OUTPUT);
+  pinMode(dirPin2, OUTPUT);
+  pinMode(enPin2, OUTPUT);
+
+  pinMode(stepPin3, OUTPUT);
+  pinMode(dirPin3, OUTPUT);
+  pinMode(enPin3, OUTPUT);
+
+  digitalWrite(enPin1, LOW); //enables the driver
+  digitalWrite(enPin2, LOW);
+  digitalWrite(enPin3, LOW); 
+}
+
+void loop() {
+  //Enables motor to move clockwise or anticlockwise
+  digitalWrite(dirPin1, LOW);
+  digitalWrite(dirPin2, LOW);
+  digitalWrite(dirPin3, LOW);
+  
+  for(int i = 0; i < 400; i++) {
+    digitalWrite(stepPin1, HIGH);
+    digitalWrite(stepPin1, LOW);
+    digitalWrite(stepPin2, HIGH);
+    digitalWrite(stepPin2, LOW);
+    digitalWrite(stepPin3, HIGH);
+    digitalWrite(stepPin3, LOW);
+    delayMicroseconds(300);
+  }
+  delay(50);
+
+  digitalWrite(dirPin1, HIGH);
+  digitalWrite(dirPin2, HIGH);
+  digitalWrite(dirPin3, HIGH);
+  S
+  for(int i = 0; i < 400; i++) {
+    digitalWrite(stepPin1, HIGH);
+    digitalWrite(stepPin1, LOW);
+    digitalWrite(stepPin2, HIGH);
+    digitalWrite(stepPin2, LOW);
+    digitalWrite(stepPin3, HIGH);
+    digitalWrite(stepPin3, LOW);
+    delayMicroseconds(300);
+  }
+  delay(50);
+}
